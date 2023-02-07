9d940ddd7a4e73df72209590efa9f20d63f007e2
Arun-integrella <arun.jain@integrella.com>
2023-02-07 16:48:51 +0530

    fix setup file

--- setup.py
+++ setup.py
@@ -62,9 +62,9 @@ setup(
         'django_bitly.migrations',
     ],
     provides=['django_bitly'],
-    python_requires='>=2.7.0',
+    python_requires='>=3.7.0',
     install_requires=[
-        "Django>=1.11, <2.0",
+        "Django>=2.2, <3.0",
         'six',
         ],
 )
